b787bfa78069e7e8238ef268ebb3476a3c55ee7e
Youngjun Cho <91277834+YJCh0@users.noreply.github.com>
2025-11-04 15:14:41 +0900

    dk fix

--- main/Guild_Tools_V2/dashboard_generator.py
+++ main/Guild_Tools_V2/dashboard_generator.py
@@ -147,7 +147,7 @@ def generate_html_dashboard(csv_file, output_file="dashboard.html", detailed_dir
     
     classes = [c['Class'] for c in characters]
     class_colors = {
-        'Death Knight':'#C41E3A','Demon Hunter':'#A330C9','Druid':'#FF7C0A',
+        'Deathknight':'#C41E3A','Demon Hunter':'#A330C9','Druid':'#FF7C0A',
         'Evoker':'#33937F','Hunter':'#AAD372','Mage':'#3FC7EB',
         'Monk':'#00FF98','Paladin':'#F48CBA','Priest':'#FFFFFF',
         'Rogue':'#FFF468','Shaman':'#0070DD','Warlock':'#8788EE',
